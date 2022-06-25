559cc9e860c24e214eb767810efc80063fdb60c0
Ralph1899 <78357991+Ralph1899@users.noreply.github.com>
2022-06-25 21:06:16 +0200

    added PID class

---
+++ PID/MPU9250/inc/pid.h
@@ -0,0 +1,25 @@
+#ifndef PID_H
+#define PID_H
+
+class PID
+{
+private:
+    double mKp, mKi, mKd;
+    float mError;
+    int mStep, mMotorDelay;
+public:
+    PID();
+    PID(double Kp, double Ki, double Kd);
+    ~PID() { };
+
+    void setP(double Kp);
+    void setI(double Ki);
+    void setD(double Kd);
+    void setPID(double Kp, double Ki, double Kd);
+    int getStep();
+    int getMotorDelay();
+
+    void runPID(double filteredInput, double desiredAngle, double dT);
+};
+
+#endif // !PID_H
---
+++ PID/MPU9250/src/pid.cpp
@@ -0,0 +1,71 @@
+#include "../inc/pid.h"
+
+PID::PID()
+    : mKp(0), mKi(0), mKd(0), mError(0) { }
+
+PID::PID(double Kp, double Ki, double Kd)
+    : mKp(Kp), mKi(Ki), mKd(Kd), mError(0) { }
+
+void PID::setP(double Kp)
+{
+    mKp = Kp;
+}
+
+void PID::setI(double Ki)
+{
+    mKi = Ki;
+}
+
+void PID::setD(double Kd)
+{
+    mKd = Kd;
+}
+
+void PID::setPID(double Kp, double Ki, double Kd)
+{
+    mKp = Kp;
+    mKi = Ki;
+    mKd = Kd;
+}
+
+int PID::getStep()
+{
+    return mStep;
+}
+
+int PID::getMotorDelay()
+{
+    return mMotorDelay;
+}
+
+void PID::runPID(double filteredInput, double desiredAngle, double dT)
+{
+    float kp, ki, kd, k_pid;
+
+    float error = filteredInput - desiredAngle;
+    kp = mKp * error;
+
+    if((error < 5) && (error > -5))
+        ki += (mKi * error);
+    else
+        ki = 0;
+
+    kd = mKd * ((error - mError) / dT);
+    mError = error;
+
+    k_pid = kp + ki + kd;
+
+    if(error > 0)
+        mStep = -1;
+    else
+        mStep = 1;
+
+    if((k_pid < 5) && (k_pid > -5))
+        mMotorDelay = 100000;
+    else
+        mMotorDelay = abs(mMotorDelay - abs(k_pid));
+    
+    if(mMotorDelay < 1000)
+        mMotorDelay = 1000;
+
+}

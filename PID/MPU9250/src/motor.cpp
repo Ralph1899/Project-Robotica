23e3e0ec59a27739afd59cecf52216c10eea0bc4
Ralph1899 <78357991+Ralph1899@users.noreply.github.com>
2022-06-26 16:02:34 +0200

    motor

--- PID/MPU9250/src/motor.cpp
+++ PID/MPU9250/src/motor.cpp
@@ -1,1 +1,38 @@
 #include "../inc/motor.h"
+
+Motor::Motor()
+    : mAngle(1000)
+{
+    std::cout << "Default ";
+    setupServo(4);
+}
+
+Motor::Motor(int gpioPin)
+    : mAngle(1000)
+{
+    setupServo(gpioPin);
+}
+
+void Motor::setupServo(int gpioPin)
+{
+    std::cout << "GPIO pin is set to " << gpioPin << "!\n";
+    mGPIO = gpioPin;
+    std::cout << "Setting servo to starting angle " << mAngle << "!\n";
+    updateServo();
+}
+
+void Motor::updateServo()
+{
+    gpioServo(mGPIO, mAngle);
+}
+
+void Motor::setGpioPin(int gpioPin)
+{
+    mGPIO = gpioPin;
+}
+
+void Motor::setAngle(int angle)
+{
+    mAngle = angle;
+    updateServo();
+}

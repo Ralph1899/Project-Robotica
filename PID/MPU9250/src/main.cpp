8fa7de03d178b988ddb162bca0269dcb776ff810
Ralph1899 <78357991+Ralph1899@users.noreply.github.com>
2022-06-27 12:09:00 +0200

    sleep

--- PID/MPU9250/src/main.cpp
+++ PID/MPU9250/src/main.cpp
@@ -130,6 +130,7 @@ int main()
 
     double compRoll, compPitch/*, gyroYaw*/;
     
+    clock::sleep_milliseconds(1000);
     while (isRunning)
     {
         // Send trigger to sensor to update values in memory
